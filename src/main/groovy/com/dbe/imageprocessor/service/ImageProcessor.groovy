dc41347a32c83b3392bce28136070876c4a8a0f7
Detlef Brendle <detlef.brendle@prospective.ch>
2018-09-03 19:41:35 +0200

    use for loops

--- src/main/groovy/com/dbe/imageprocessor/service/ImageProcessor.groovy
+++ src/main/groovy/com/dbe/imageprocessor/service/ImageProcessor.groovy
@@ -4,7 +4,9 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 
 interface ImageProcessor {
-    Map<Color, Long> groupColors(BufferedImage image)
+    Map<Color, Long> groupColors(BufferedImage image,Integer colorCount)
 
     BufferedImage reduceColors(BufferedImage image, Integer colorCount)
+
+    BufferedImage generateMinimalBlocks(BufferedImage bufferedImage, int x, int y)
 }
