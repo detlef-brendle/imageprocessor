fd01fa3eaf5d289b96d24c229210e473f22a213b
Detlef Brendle <detlef.brendle@prospective.ch>
2018-09-03 19:45:28 +0200

    use for loops

--- src/main/groovy/com/dbe/imageprocessor/service/ImageProcessorImpl.groovy
+++ src/main/groovy/com/dbe/imageprocessor/service/ImageProcessorImpl.groovy
@@ -1,26 +1,44 @@
 package com.dbe.imageprocessor.service
 
-
 import java.awt.*
 import java.awt.image.BufferedImage
 
+import static java.lang.Math.pow
+import static java.lang.Math.sqrt
+
 class ImageProcessorImpl implements ImageProcessor {
     @Override
-    Map<Color, Long> groupColors(BufferedImage image) {
+    Map<Color, Long> groupColors(BufferedImage image, Integer colorCount) {
         Map<Color, Long> colorsMap = [:]
-        Map<Color, Color> equivalentColors = [:]
-        image.width.times { w ->
-            image.height.times { h ->
-                Color sourceColor = new Color(image.getRGB(w, h))
-                Color c = searchColor(sourceColor, colorsMap.keySet(), equivalentColors)
-                colorsMap.putIfAbsent(c, 1)
-                colorsMap.put(c, colorsMap.get(c) + 1)
+        def distance = 100
+        def checkedDistance = []
+        while (colorsMap.size() != colorCount) {
+            println "check with distance $distance"
+            if (checkedDistance.contains(distance)) {
+                break
+            }
+            colorsMap.clear()
+            Map<Color, Color> equivalentColors = [:]
+            for (int w = 0; w < image.width; w++) {
+                for (int h = 0; h < image.height; h++) {
+                    Color c = searchColor(image, w, h, colorsMap.keySet(), equivalentColors, distance)
+                    colorsMap.putIfAbsent(c, 1)
+                    colorsMap.put(c, colorsMap.get(c) + 1)
+                }
+            }
+            checkedDistance << distance
+            if (colorsMap.size() > colorCount) {
+                distance = distance + colorsMap.size() - colorCount
+            } else if (colorsMap.size() < colorCount) {
+                distance = distance - colorCount - colorsMap.size()
             }
         }
+        println "final distance was $distance"
         return colorsMap.sort { v -> v.value * -1 }
     }
 
-    Color searchColor(Color sourceColor, Set colors, Map equivalentColors) {
+    Color searchColor(image, w, h, Set colors, Map equivalentColors, d) {
+        Color sourceColor = new Color(image.getRGB(w, h))
         Color equivalentColor = null
         if (equivalentColors.containsKey(sourceColor)) {
             equivalentColor = equivalentColors.get(sourceColor)
@@ -28,10 +46,10 @@ class ImageProcessorImpl implements ImageProcessor {
         def iter = colors.iterator()
         while (equivalentColor == null && iter.hasNext()) {
             Color c = iter.next()
-            def distance = Math.sqrt(Math.pow(c.red - sourceColor.red, 2) + Math.pow(c.green - sourceColor.green, 2) + Math.pow(c.blue - sourceColor.blue, 2))
-            if (distance < 40) {
+            def distance = sqrt(pow(c.red - sourceColor.red, 2) + pow(c.green - sourceColor.green, 2) + pow(c.blue - sourceColor.blue, 2))
+            if (distance < d) {
                 equivalentColor = c
-                equivalentColors.put(sourceColor,c)
+                equivalentColors.put(sourceColor, c)
             }
         }
         equivalentColor != null ? equivalentColor : sourceColor
@@ -40,11 +58,12 @@ class ImageProcessorImpl implements ImageProcessor {
     @Override
     BufferedImage reduceColors(BufferedImage image, Integer colorCount) {
         BufferedImage img = new BufferedImage(image.width, image.height, image.type)
-        def colors = groupColors(image)
+        def colors = groupColors(image, colorCount)
         def targetColors = colors.take(colorCount)
         Map<Integer, Integer> colorsCache = [:]
-        image.width.times { w ->
-            image.height.times { h ->
+
+        for (int w = 0; w < image.width; w++) {
+            for (int h = 0; h < image.height; h++) {
                 def sourceRGB = image.getRGB(w, h)
                 def newColor
                 if (!colorsCache.containsKey(sourceRGB)) {
@@ -59,12 +78,23 @@ class ImageProcessorImpl implements ImageProcessor {
         return img
     }
 
+
+    @Override
+    BufferedImage generateMinimalBlocks(BufferedImage bufferedImage, int x, int y) {
+        BufferedImage result = new BufferedImage(image.width, image.height, image.type)
+        bufferedImage.width.times { w ->
+            bufferedImage.height.times { h ->
+                bufferedImage.getRGB(w, h)
+            }
+        }
+    }
+
     private findNearestNewColor(Set<Color> targetColors, int color) {
         Color nearestTargetColor = null;
         Integer nearestDistance = new Integer(Integer.MAX_VALUE)
         Color sourceColor = new Color(color)
         targetColors.each { Color c ->
-            def distance = Math.sqrt(Math.pow(c.red - sourceColor.red, 2) + Math.pow(c.green - sourceColor.green, 2) + Math.pow(c.blue - sourceColor.blue, 2))
+            def distance = sqrt(pow(c.red - sourceColor.red, 2) + pow(c.green - sourceColor.green, 2) + pow(c.blue - sourceColor.blue, 2))
 
             if (nearestDistance > distance) {
                 nearestTargetColor = c
@@ -75,4 +105,8 @@ class ImageProcessorImpl implements ImageProcessor {
         return nearestTargetColor.RGB
     }
 
+    BufferedImage drawLines(BufferedImage image) {
+        def subimage = image.getSubimage(0, 0, 10, 10)
+        println subimage
+    }
 }
--- src/test/groovy/com/dbe/imageprocessor/service/ImageProcessorImplTest.groovy
+++ src/test/groovy/com/dbe/imageprocessor/service/ImageProcessorImplTest.groovy
@@ -1,23 +1,65 @@
 package com.dbe.imageprocessor.service
 
+import com.jhlabs.composite.MultiplyComposite
+import com.jhlabs.image.CompositeFilter
+import com.jhlabs.image.EdgeFilter
+import com.jhlabs.image.GrayFilter
+import com.jhlabs.image.GrayscaleFilter
+import com.jhlabs.image.InvertFilter
+import org.springframework.core.io.ClassPathResource
 import spock.lang.Specification
 
 import javax.imageio.ImageIO
-import java.util.function.Consumer
+import java.awt.image.BufferedImage
 
 class ImageProcessorImplTest extends Specification {
-    def 'process image'() {
-        when:
-        (4..14).parallelStream().forEach(new Consumer() {
-            @Override
-            void accept(def it) {
-                def newImage = new ImageProcessorImpl().reduceColors(ImageIO.read(new File('/home/detlef/Bilder/2018/06/10/DSC06883.JPG')), it)
-                ImageIO.write(newImage, "png", new File("/home/detlef/temp/newimage${it}.png"))
-                println "write file $it"
-            }
-        })
 
+    def 'process image'() {
+        setup:
+        def outputDir = new File('output')
+        outputDir.deleteDir()
+        outputDir.mkdir()
+        def imageprocessor = new ImageProcessorImpl()
+        when:
+        def newImage = imageprocessor.reduceColors(ImageIO.read(new ClassPathResource('images/diego.png').inputStream), 10)
+        newImage = imageprocessor.drawLines(newImage)
+        ImageIO.write(newImage, "png", new File(outputDir, 'diego-processed.png'))
         then: ''
     }
 
+    def 'test with real file'() {
+        when:
+        def newImage = new ImageProcessorImpl().reduceColors(ImageIO.read(new File('/home/detlef/Bilder/2017/12/03/DSC06826.JPG')), 10)
+        ImageIO.write(newImage, "jpg", new File('/home/detlef/Bilder/2017/12/03/DSC06826-p.JPG'))
+        then: ''
+    }
+
+    def 'subimage'() {
+        def src = ImageIO.read(new File('output/diego-processed.png'))
+        def dst = new BufferedImage(src.width, src.height, src.type)
+        EdgeFilter filter = new EdgeFilter()
+        GrayscaleFilter grayFilter=new GrayscaleFilter()
+        InvertFilter invertFilter =new InvertFilter()
+        CompositeFilter compositeFilter=new CompositeFilter(new MultiplyComposite(1))
+
+        filter
+        when:
+        filter.filter(src, dst)
+        grayFilter.filter(dst,dst)
+        invertFilter.filter(dst,dst)
+        compositeFilter.filter(src,dst)
+        ImageIO.write(dst, "jpg", new File('output/diego-edge.png'))
+        then: ''
+    }
+
+    def 'edge filter'() {
+        when:
+        def src = ImageIO.read(new File('/home/detlef/Bilder/2017/12/03/DSC06826-p.JPG'))
+        def dst = new BufferedImage(src.width, src.height, src.type)
+        EdgeFilter filter = new EdgeFilter()
+        filter.filter(src,dst)
+        ImageIO.write(dst, "jpg", new File('output/diego-edge.png'))
+        then:''
+
+    }
 }
